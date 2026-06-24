86f69553ae9fd126436422c2e4653e3a234d00fd
Justin Pacella <jtp75@pitt.edu>
2026-06-24 12:33:10 -0400

    chore: clean up deleted files

--- crates/vettd-skill-scanner/src/chain.rs
+++
@@ -1,4 +0,0 @@
-// This stub has been replaced by checks/chains.rs which contains the real
-// implementations of detect_exfiltration_chains and detect_malicious_activity_chains.
-//
-// This file is no longer compiled. It can be deleted.
